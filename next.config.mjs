02b2cb6f91679e460e9b663e8363bda5b8ccf509
Deborah Sanchez <deborah@quarksalchemist.com>
2024-06-13 13:55:55 +0800

    nuevo next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' *",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
