a705e039e6a2b37c9a84d8cc2be7844ce1950a88
Deborah Sanchez <deborah@quarksalchemist.com>
2024-07-01 17:57:53 +0800

    colapsable flecha

--- src/components/home/mobile-hero.js
+++ src/components/home/mobile-hero.js
@@ -328,7 +328,7 @@ function MobileHero() {
                       <Box
                         sx={{
                           position: "absolute",
-                          right: "10%",
+                          right: "-5vw",
                           top: -3,
                         }}
                       >
@@ -344,7 +344,7 @@ function MobileHero() {
                       <Box
                         sx={{
                           position: "absolute",
-                          right: "10%",
+                          right: "-5vw",
                           top: -3,
                         }}
                       >
